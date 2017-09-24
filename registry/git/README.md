3b0ce8a2b79bde8417d6e67aadad6f761e6cc87b
blacktop <blacktop@users.noreply.github.com>
2017-09-24 17:54:27 -0600

    update metrics

--- registry/git/README.md
+++ registry/git/README.md
@@ -11,6 +11,8 @@ Here is the **alpine** package: https://pkgs.alpinelinux.org/package/v3.6/main/a
 Example
 -------
 
+Clone `maliceio/registry`
+
 ```go
 package main
 
@@ -39,4 +41,49 @@ func main() {
 DEBU[0000] malice registry has been cloned into: ~/.malice/registry
 DEBU[0000] good signature from: blacktop <blacktop@users.noreply.github.com>
 DEBU[0000] malice registry has been updated
+```
+
+Get malice plugin **metrics**
+
+```go
+package main
+
+import "github.com/maliceio/engine/registry/git"
+
+func main() {
+	git.GetMetricsForRepo("")
+}
+```
+
+```sh
+plugin: fileinfo, stars: 9
+plugin: yara, stars: 9
+plugin: exe, stars: 7
+plugin: nsrl, stars: 4
+plugin: virustotal, stars: 4
+plugin: office, stars: 4
+plugin: pdf, stars: 4
+plugin: windows-defender, stars: 3
+plugin: floss, stars: 3
+plugin: malice-alpine, stars: 2
+plugin: fprot, stars: 2
+plugin: bro, stars: 2
+plugin: javascript, stars: 2
+plugin: go-plugin-utils, stars: 2
+plugin: totalhash, stars: 2
+plugin: fsecure, stars: 1
+plugin: team-cymru, stars: 1
+plugin: threat-expert, stars: 1
+plugin: sophos, stars: 1
+plugin: anubis, stars: 1
+plugin: archive, stars: 1
+plugin: comodo, stars: 1
+plugin: clamav, stars: 1
+plugin: bitdefender, stars: 1
+plugin: avg, stars: 1
+plugin: avast, stars: 1
+plugin: shadow-server, stars: 1
+plugin: avira, stars: 1
+plugin: zoner, stars: 1
+plugin: escan, stars: 1
 ```
---
+++ registry/git/metrics.go
@@ -0,0 +1,60 @@
+package git
+
+import (
+	"fmt"
+	"sort"
+	"time"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/net/context"
+)
+
+const timeout = 3
+
+// RepoMetric the repo metrics struct
+type RepoMetric struct {
+	Name        string `json:"name,omitempty"`
+	Description string `json:"description,omitempty"`
+	Stars       int    `json:"stars,omitempty"`
+	Forks       int    `json:"forks,omitempty"`
+	Watchers    int    `json:"watchers,omitempty"`
+}
+
+// GetMetricsForRepo reads plugin metrics from Github API
+func GetMetricsForRepo(repo string) (RepoMetric, error) {
+	rm := RepoMetric{}
+	rms := []RepoMetric{}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(timeout)*time.Second)
+	defer cancel()
+
+	client := github.NewClient(nil)
+
+	// list public repositories for org "github"
+	opt := &github.RepositoryListByOrgOptions{Type: "public"}
+	repos, _, err := client.Repositories.ListByOrg(ctx, "malice-plugins", opt)
+	if err != nil {
+		return rm, err
+	}
+
+	for _, repo := range repos {
+		rm := RepoMetric{}
+		rm.Name = repo.GetName()
+		rm.Description = repo.GetDescription()
+		rm.Stars = repo.GetStargazersCount()
+		rm.Forks = repo.GetForksCount()
+		rm.Watchers = repo.GetWatchersCount()
+		// add to slice
+		rms = append(rms, rm)
+	}
+
+	sort.Slice(rms, func(i, j int) bool {
+		return rms[i].Stars > rms[j].Stars
+	})
+
+	for _, r := range rms {
+		fmt.Printf("plugin: %s, stars: %d\n", r.Name, r.Stars)
+	}
+
+	return rm, nil
+}
